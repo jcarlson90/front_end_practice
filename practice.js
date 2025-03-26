e3da543d865294bfc844b9cd14c8f06ed106a5ec
jcarlson <jake.carlson90@gmail.com>
2025-03-26 11:36:54 -0700

    more

--- practice.js
+++ practice.js
@@ -7,13 +7,32 @@ function reversedString(str) {
     return reversed;
 }
 
-console.log(reversedString("coding"));
+// console.log(reversedString("jake"));
 
 function isPalindrome(str) {
     const reversed = str.split("").reverse().join("");
     return str === reversed;
 }
 
-console.log(isPalindrome("racecar"));
-console.log(isPalindrome("hellow"));
+// console.log(isPalindrome("racecar"));
+// console.log(isPalindrome("hello"));
+
+function largestNum(nums) {
+    let largest = nums[0];
+    for ( let i = 1; i < nums.length; i++) {
+        if (nums[i] > largest) {
+            largest = nums[i];
+        }
+    }
+    return largest;
+}
+
+// console.log(largestNum([1, 4, 8, 24, 97, 43]));
+
+function removeDuplicates(nums) {
+    return [...new Set(nums)]; //uses SET javascript object doesnt allow duplicates.
+}
+
+// console.log(removeDuplicates([1, 1, 2, 2, 3, 4, 5, 5, 6, 7]));
+
 
