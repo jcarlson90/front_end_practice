fd79ccd7167ef869bd50b96a280803695453a000
jcarlson <jake.carlson90@gmail.com>
2025-03-31 13:50:43 -0700

    firstNonRepeatCharacter

--- practice.js
+++ practice.js
@@ -35,4 +35,40 @@ function removeDuplicates(nums) {
 
 // console.log(removeDuplicates([1, 1, 2, 2, 3, 4, 5, 5, 6, 7]));
 
+function mergeArrays(arr1, arr2) {
+    let result = [];
+    let i = 0, j= 0;
+
+    while (i < arr1.legnth && j < arr2.legnth) {
+        if (arr1[i] < arr2[j]){
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++
+        }
+    }
+    return result.concat(arr1.slice(i)).concat(arr2.slice(j));
+}
+
+// console.log(mergeArrays([1, 3, 5], [2, 4, 6]));
+
+function firstNonRepeatChar(str) {
+    let charCount = {};
+
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) +1;
+    }
+
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
+}
+
+console.log(firstNonRepeatChar("aabbcddce"));
+
+
 
